e127089c93b1d43b37f7a52a06426f438f820fe9
abandom_L <tj_grgzlt@163.com>
2017-06-11 21:00:37 +0800

    delete password

--- Thearchy/ThearchyAthena/src/main/java/com/abandom/sk/bean/User.java
+++ Thearchy/ThearchyAthena/src/main/java/com/abandom/sk/bean/User.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 public class User {
     private String id;
     private String username;
-    private String password;
 
     public String getUsername() {
         return username;
@@ -17,14 +16,6 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getId() {
@@ -44,7 +35,6 @@ public class User {
         return "User{" +
                 "id='" + id + '\'' +
                 ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 '}';
     }
 }
